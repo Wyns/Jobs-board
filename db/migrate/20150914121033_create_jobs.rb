a2fdfe3a801234884a2b76dbc3ee398c4d41a637
Wyns <pauwains@gmail.com>
2015-09-14 15:14:46 +0300

    added title and description to the jobs table

--- db/migrate/20150914121033_create_jobs.rb
+++ db/migrate/20150914121033_create_jobs.rb
@@ -1,7 +1,8 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-
+      t.text :title
+        t.text :description
       t.timestamps null: false
     end
   end
